b1506f0c41e58d8d9a8ed679b29b0d5755967cef
Swarooptheja <swarooptheja809@gmail.com>
2023-02-14 23:43:12 +0530

    added

--- src/components/Healthcheck/Healthsinglepage.jsx
+++ src/components/Healthcheck/Healthsinglepage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Healthsinglepage.css"
 import {TbTestPipe} from "react-icons/tb"
-import {ImSmile2} from "react-icons/im"
+import {ImSmile2} from "react-icons/im";
+import Caurosel from "../Caurousel2";
+import { BsFillArrowRightCircleFill } from 'react-icons/bs';
+import { TransitionExample } from '../Modal';
 export const Healthsinglepage = () => {
-    let data=JSON.parse(localStorage.getItem("singledata"))||[]
+    let data=JSON.parse(localStorage.getItem("singledata"))||[];
+    let [state,setstate]=useState(0)
+    let handle=()=>{
+        setstate((state)=>state+1)
+    }
+    useEffect(()=>{
+
+    },state)
   return (
     <>
     <h1 style={{width:"90%",textAlign:"left", margin:"auto"}}>{data.name}</h1>
@@ -20,8 +30,12 @@ export const Healthsinglepage = () => {
                     <ImSmile2 size={50}  className="icon" color={"#E349C2"}/>
                 </div>
             </div>
-            <div>
-
+            <div className="icon-container">
+              <img src="https://assets.mfine.co/api/contentservice/attachments/download/catalog-manager/catalog-item/images/Why_hypertension_1639561444116.jpg/w_150,h_150" alt="" />
+              <button style={{backgroundColor:"white", border:"none"}}>
+                <TransitionExample/>
+               {/* <BsFillArrowRightCircleFill size={50} className="icon" cursor={"pointer"} color={"orangered"} onClick={handle}/> */}
+              </button>
             </div>
             <div id='secondthird'>
                 <div>
@@ -37,6 +51,14 @@ export const Healthsinglepage = () => {
                     <p>Online report within 48 hours</p>
                 </div>
             </div>
+        </div>
+    </div>
+    <div id='singlebottom'>
+        <div>
+            <h1>{data.name}</h1>
+        </div>
+        <div>
+            <button>Book Now</button>
         </div>
     </div>
     </>
